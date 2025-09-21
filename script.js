6e91e04d04dc54d1965805e334272b01c0b40a6f
saad-pie <saadabdulrehman7474@gmail.com>
2025-09-21 19:41:43 +0500

    feat: add script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded', () => {const navToggle = document.querySelector('.nav-toggle');const navLinks = document.querySelector('.nav-links');if (navToggle && navLinks) {navToggle.addEventListener('click', () => {navLinks.classList.toggle('nav-active');});}}); 
